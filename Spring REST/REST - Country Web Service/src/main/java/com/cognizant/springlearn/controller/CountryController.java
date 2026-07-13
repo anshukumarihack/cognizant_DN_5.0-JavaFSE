8127b5be1aca0eb710d171bfb28592c8bc5f0ed2
AnshukumariKumar <vtu24311@veltech.edu.in>
2026-07-13 14:52:41 +0530

    Remove unused ApplicationContext imports after context leak fix

--- Spring REST/REST - Country Web Service/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ Spring REST/REST - Country Web Service/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -2,7 +2,6 @@ package com.cognizant.springlearn.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
--- Spring REST/REST - Get country based on country code/src/main/java/com/cognizant/springlearn/service/CountryService.java
+++ Spring REST/REST - Get country based on country code/src/main/java/com/cognizant/springlearn/service/CountryService.java
@@ -3,7 +3,6 @@ package com.cognizant.springlearn.service;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 import com.cognizant.springlearn.Country;
